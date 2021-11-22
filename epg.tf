c3d7ec3c595b499293acecf6c3cecba65212faff
Camillo <camrossi@cisco.com>
2021-11-22 15:55:46 +1100

    dddd

--- epg.tf
+++ epg.tf
@@ -19,7 +19,7 @@ provider "aci" {
 }
 
 resource "aci_application_profile" "app" {
-  tenant_dn = data.terraform_remote_state.tenant_id
+  tenant_dn = data.terraform_remote_state.networking.tenant_id
   name      = var.app
 }
 
@@ -27,7 +27,7 @@ resource "aci_application_epg" "epgs" {
   count = length(var.epgs)
   name  = var.epgs[count.index]
   application_profile_dn = aci_application_profile.app.id
-  relation_fv_rs_bd = data.terraform_remote_state.bd_id
+  relation_fv_rs_bd = data.terraform_remote_state.networking.bd_id
 }
 
 
